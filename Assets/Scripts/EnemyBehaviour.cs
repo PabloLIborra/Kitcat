0b8827ec6ff2dad13f6b850ba8a38a00d7c6a318
Adrián Francés Lillo <adrianfranceslillo@gmail.com>
2018-07-04 13:24:45 +0200

    Added second direction to enemy

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class EnemyBehaviour : MonoBehaviour {
     public float velocity = 1.0f;
     public int direction = 1;
+    public bool activated = false;
+    public float time = 0.0f;
+    public float activeTime = 5.0f;
     Transform t;
     // Use this for initialization
     void Start () {
@@ -19,7 +22,16 @@ public class EnemyBehaviour : MonoBehaviour {
         {
             t.position = new Vector3(t.position.x + velocity * Time.deltaTime, t.position.y, t.position.z);
 
-            if(t.position.x > 0)
+            if(t.position.x > -2)
+            {
+                Destroy(gameObject);
+            }
+        }
+        if(direction == 2)
+        {
+            t.position = new Vector3(t.position.x - velocity * Time.deltaTime, t.position.y, t.position.z);
+
+            if(t.position.x < 3)
             {
                 Destroy(gameObject);
             }
