1a592818de0fd95f68ce5e1003ce612416a23b91
Adrián Francés Lillo <adrianfranceslillo@gmail.com>
2018-07-04 12:35:08 +0200

    Added Enemy Basic Behaviour

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour {
+    public float velocity = 1.0f;
+    public int direction = 1;
+    Transform t;
+    // Use this for initialization
+    void Start () {
+        t = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Move from left to right
+		if(direction == 1)
+        {
+            t.position = new Vector3(t.position.x + velocity * Time.deltaTime, t.position.y, t.position.z);
+
+            if(t.position.x > 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+	}
+}
