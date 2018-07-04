c9ea7e2af0287b568cb748f429c8b56e3d184084
donluispanis <lga42@gcloud.ua.es>
2018-07-04 22:30:04 +0200

    Items are now visible

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -8,12 +8,41 @@ public class Piece : MonoBehaviour {
     public List<string> gestures = new List<string>();
     public bool completed = false;
     public bool active = false;
-
     public Weapon weapon;
+
+    public List<GameObject> sprites;
 
     // Use this for initialization
     void Start () {
-		
+        float x = transform.position.x - totalGestures.Count * 0.5f + 0.5f;
+        float y = transform.position.y + 1;
+
+        for (int i = 0; i < totalGestures.Count; i++)
+        {
+            GameObject g = new GameObject();
+            g.transform.position = new Vector3(x + i, y, 0f);
+            SpriteRenderer r = g.AddComponent<SpriteRenderer>();
+
+            switch (totalGestures[i]) {
+                case "a":
+                    r.sprite = Resources.Load<Sprite>("Sprites/Symbols/a");
+                    break;
+                case "v":
+                    r.sprite = Resources.Load<Sprite>("Sprites/Symbols/v");
+                    break;
+                case "circulo":
+                    r.sprite = Resources.Load<Sprite>("Sprites/Symbols/circulo");
+                    break;
+                case "horizontal":
+                    r.sprite = Resources.Load<Sprite>("Sprites/Symbols/horizontal");
+                    break;
+                case "vertical":
+                    r.sprite = Resources.Load<Sprite>("Sprites/Symbols/vertical");
+                    break;
+            }
+
+            sprites.Add(g);
+        }
 	}
 	
 	// Update is called once per frame
@@ -34,6 +63,14 @@ public class Piece : MonoBehaviour {
         if (gestureNameDer == name || gestureNameIzq == name)
         {
             gestures.Add(name);
+            Destroy(sprites[0]);
+            sprites.RemoveAt(0);
+
+            for(int i = 0; i < sprites.Count; i++)
+            {
+                Vector3 t = sprites[i].transform.position;
+                sprites[i].transform.position = new Vector3(t.x - 0.5f, t.y, 0.0f);
+            }
         }
     }
 }
